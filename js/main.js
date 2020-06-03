ec85db53163c5e2de2ef901e42276beac3192207
Dernival Liandro <liandro.sys@gmail.com>
2020-06-02 22:02:15 -0300

    Criação do projeto

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Dernival Liandro";
+var n1 = 15;
+var n2 = 10;
+var frase = "Testando o console.log";
+
+console.log(nome)
+console.log(15 * 10)
+console.log(frase.toLocaleLowerCase())
+console.log(frase.toLocaleUpperCase())
+console.log(frase.replace("Testando", "Usando"))
