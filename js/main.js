d8b074795e0fd28005177bd15bfe368b24f71ae8
Dernival Liandro <liandro.sys@gmail.com>
2020-06-02 22:24:40 -0300

    Arrays e Dicionários

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
-var nome = "Dernival Liandro";
-var n1 = 15;
-var n2 = 10;
-var frase = "Testando o console.log";
+//var nome = "Dernival Liandro";
+//var n1 = 15;
+//var n2 = 10;
+//var frase = "Testando o console.log";
 
-console.log(nome)
-console.log(15 * 10)
-console.log(frase.toLocaleLowerCase())
-console.log(frase.toLocaleUpperCase())
-console.log(frase.replace("Testando", "Usando"))
+//console.log(nome)
+//console.log(15 * 10)
+//console.log(frase.toLocaleLowerCase())
+//console.log(frase.toLocaleUpperCase())
+//console.log(frase.replace("Testando", "Usando"))
+
+
+//var lista = ["maçã", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+/*
+var fruta = {nome: "maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].cor);
